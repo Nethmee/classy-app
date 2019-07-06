c289b06aab4df36a4a6ea5a639b8fc35291497cf
Nethmee <nssellahewa@gmail.com>
2019-07-06 16:07:14 +0530

    init commit

---
+++ main.js
@@ -0,0 +1,10 @@
+alert("hii there!!");
+
+
+function f2(){
+
+	var x=20;
+	alert(x*5);
+} 
+
+f2();
---
+++ newLetter.js
@@ -0,0 +1,26 @@
+function f2(){
+
+var name=document.getElementById("User").value;
+var email=document.getElementById("em").value;
+var pass=document.getElementById("pass").value;
+
+if(pass=="admin"){
+
+alert("give a password");
+
+
+}
+else{
+	alert("you are not an admin");
+}
+/*if(User!=null|| email!=null ||pass!=null){
+	alert("Thank you  " +email+  "  for subscribing to our news letter.");
+	//document.writeln(" email is"+email);
+}
+else{
+
+	alert("wrong submission.");
+
+}*/
+//document.writeln("hi");
+}
